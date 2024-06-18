69961ef9db305a9981b7b95a49b1f8f23cd3ca57
Singhvivek78 <viveksingh60967@gmail.com>
2024-06-18 14:05:01 +0530

    javascript

---
+++ conditions.js
@@ -0,0 +1,14 @@
+n=782;
+if (n%2===0) {
+    console.log('even')
+   a='something'; 
+   let b='better';
+   const pi=3.1416;
+//    pi=43.14;
+
+}
+else{
+    console.log('odd')
+}
+console.log(a);
+// console.log(b);
---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hello JavaScript');
+a=4654;
+console.log(a);
+console.log(typeof a);
+
+myname="vivek";
+console.log(myname);
+// exponent operator
+console.log(2**5);
+console.log(1=='1');
+console.log(1==='1');
